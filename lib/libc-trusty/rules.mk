3fbcaf7863e37982fc42078b9a66a3a39d2d3976
Michael Ryleev <gmar@google.com>
2015-05-13 19:40:05 -0700

    [lib][libc-trusty] Reimplement assert to decouple it from LK assert
    In addition implement __libc_fatal and __libc_fatal_no_abort that
    intended to be used internally.
    
    Switch to use new assert implementation.
    
    Change-Id: Ib30293e7c2fde7212297ed00fc872c7662e8f357

--- lib/libc-trusty/rules.mk
+++ lib/libc-trusty/rules.mk
@@ -4,13 +4,17 @@ MODULE := $(LOCAL_DIR)
 
 WITH_CUSTOM_MALLOC := true
 
+GLOBAL_INCLUDES := $(LOCAL_DIR)/include $(LKROOT)/include $(GLOBAL_INCLUDES)
+
 MODULE_SRCS := \
 	$(LOCAL_DIR)/abort.c \
+	$(LOCAL_DIR)/assert.c \
 	$(LOCAL_DIR)/atexit.c \
 	$(LOCAL_DIR)/exit.c \
 	$(LOCAL_DIR)/malloc.c \
 	$(LOCAL_DIR)/stdio.c \
 	$(LOCAL_DIR)/libc_init.c \
+	$(LOCAL_DIR)/libc_fatal.c \
 
 include $(LOCAL_DIR)/arch/$(ARCH)/rules.mk
 
